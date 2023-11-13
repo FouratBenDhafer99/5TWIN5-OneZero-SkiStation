a43fd61483f1b3af01e839bba279a7cff19904c5
Rayen Bourguiba <rayen.bourguiba@esprit.tn>
2023-11-13 16:12:29 +0100

    Update SkierServicesMockitoTest.java

--- src/test/java/tn/esprit/spring/SkierServicesMockitoTest.java
+++ src/test/java/tn/esprit/spring/SkierServicesMockitoTest.java
@@ -12,6 +12,7 @@ import tn.esprit.spring.services.*;
 import java.time.LocalDate;
 import java.util.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class SkierServicesMockitoTest {
@@ -20,7 +21,10 @@ public class SkierServicesMockitoTest {
 
     @Mock
     private ISkierRepository skierRepository;
-
+    @Mock
+    private ICourseRepository courseRepository;
+    @Mock
+    private IRegistrationRepository registrationRepository;
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
@@ -52,4 +56,21 @@ public class SkierServicesMockitoTest {
         Skier result = skierServices.retrieveSkier(skierId);
         assertEquals(skier, result);
     }
+
+    @Test
+    public void testAddSkierAndAssignToCourse() {
+        Skier skier = new Skier();
+        skier.setRegistrations(new HashSet<>());
+        Long numCourse = 1L;
+        Course course = new Course();
+        course.setNumCourse(numCourse);
+        when(skierRepository.save(any(Skier.class))).thenReturn(skier);
+        when(courseRepository.getById(numCourse)).thenReturn(course);
+        Skier result = skierServices.addSkierAndAssignToCourse(skier, numCourse);
+        verify(skierRepository, times(1)).save(skier);
+        verify(courseRepository, times(1)).getById(numCourse);
+        verify(registrationRepository, times(skier.getRegistrations().size())).save(any(Registration.class));
+        assertTrue(result.getRegistrations().stream().allMatch(registration -> registration.getCourse().equals(course)));
+    }
 }
+
