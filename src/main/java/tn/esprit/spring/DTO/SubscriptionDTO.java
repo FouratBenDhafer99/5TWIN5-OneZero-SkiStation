18f9eb34a5624e6f356b1298daeb2b4a754c61d2
Ayachi Oumaima <98391753+oumaima1115@users.noreply.github.com>
2023-11-13 16:17:58 +0100

    Update SubscriptionDTO.java

--- src/main/java/tn/esprit/spring/DTO/SubscriptionDTO.java
+++ src/main/java/tn/esprit/spring/DTO/SubscriptionDTO.java
@@ -1,11 +1,8 @@
 package tn.esprit.spring.DTO;
-
-import lombok.Data;
 import tn.esprit.spring.entities.TypeSubscription;
-
 import java.time.LocalDate;
 
-@Data
+@AllArgsConstructor @Getter @Setter @NoArgsConstructor
 public class SubscriptionDTO {
     private Long NumSub;
     private LocalDate startDate;
