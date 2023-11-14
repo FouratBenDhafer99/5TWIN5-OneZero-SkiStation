86080f5ede66ca52c7775297d89c49cf93f6d6fb
Fourat Ben Dhafer <fouratbendhafer99@gmail.com>
2023-11-14 02:28:39 +0100

    Origins

--- src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
+++ src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
@@ -2,11 +2,8 @@ package tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,29 +14,4 @@ public class GestionStationSkiApplication {
 		SpringApplication.run(GestionStationSkiApplication.class, args);
 	}
 
-	@Bean
-	public static WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						//.allowedOrigins("http://192.168.1.15:5000", "http://localhost:3000")
-						.allowedOrigins("http://192.168.1.15:5000", "http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedHeaders("*");
-			}
-		};
-	}
-/*
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*"); // Allow all origins
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}*/
 }
